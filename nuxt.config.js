61d31c901465537bb92bcd0b783e58c3a5caf9cb
Hällbom Olle <olle.hallbom@scania.com>
2021-08-06 22:16:35 +0200

    fixing icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,28 @@ export default {
     titleTemplate: '%s - brukbar',
     title: 'brukbar',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -42,29 +44,22 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+        light: {
+          primary: '#009688',
+          secondary: '#607D8B',
+          anchor: '#8c9eff',
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <!-- -->
+    <v-app-bar app dense>
+      <v-tabs align-with-title>
+        <v-tab>Om oss</v-tab>
+        <v-tab>Produkter</v-tab>
+        <v-tab>Kontakt</v-tab>
+      </v-tabs>
     </v-app-bar>
 
     <v-main>
@@ -11,7 +15,13 @@
     </v-main>
 
     <v-footer app>
-      <!-- -->
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-icon large>fas fa-map-marker-alt</v-icon>
+          <v-icon class="ma-4" large>fab fa-facebook-square</v-icon>
+          <v-icon large>fab fa-instagram</v-icon>
+        </v-row>
+      </v-container>
     </v-footer>
   </v-app>
 </template>

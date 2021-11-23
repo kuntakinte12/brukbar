ddd2426e51ae5cb3c94f2af34ef8288042f1cc6c
Olle <olle.hallbom@scania.com>
2021-11-23 22:28:12 +0100

    adding seo content

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'cirkulär järnhandel',
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content: 'brukbar, cirkulär, järnhandel, enköping',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
